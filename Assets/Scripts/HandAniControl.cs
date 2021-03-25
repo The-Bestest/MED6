c2ab71861a2ed48d274fb740eccd308e94c0cdad
Oppelion <56341984+Oppelion@users.noreply.github.com>
2021-03-25 13:15:59 +0100

    Hand animation fixed
    Hand animation now works
          .--..--..--..--..--..--.
        .' \  (`._   (_)     _   \
      .'    |  '._)         (_)  |
      \ _.')\      .----..---.   /
      |(_.'  |    /    .-\-.  \  |
      \     0|    |   ( O| O) | o|
       |  _  |  .--.____.'._.-.  |
       \ (_) | o         -` .-`  |
        |    \   |`-._ _ _ _ _\ /
        \    |   |  `. |_||_|   |
        | o  |    \_      \     |     -.   .-.
        |.-.  \     `--..-'   O |     `.`-' .'
      _.'  .' |     `-.-'      /-.__   ' .-'
    .' `-.` '.|='=.='=.='=.='=|._/_ `-'.'
    `-._  `.  |________/\_____|    `-.'
       .'   ).| '=' '='\/ '=' |
       `._.`  '---------------'
               //___\   //___\
                 ||       ||
                 ||_.-.   ||_.-.
                (_.--__) (_.--__)

--- Assets/Scripts/HandAniControl.cs
+++ Assets/Scripts/HandAniControl.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class HandAniControl : MonoBehaviour
 {
+    //public OpenBCIInput openBCIInput;
     public Animator anim;
 
     float lastValue = 0;
-    float tempVal = 0;
+    float tempVal = 0.1f;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-
-        anim.SetBool("Close", true);
-        anim.SetBool("Open", false);
     }
 
     private void Update()
@@ -25,72 +23,44 @@ public class HandAniControl : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            tempVal = 0.5f;
+            tempVal = 0.3f;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            tempVal = 0.25f;
+            tempVal = 0.5f;
         }
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Open", false);
+            anim.SetBool("Close", true);
+            anim.SetBool("Start", true);
+        }
         AniControl(tempVal);
         Debug.Log(tempVal);
     }
 
     public void AniControl(float currentValue)
     {
-        // Make "tempThreshold" into "openBCIInput.classificationThreshold" when moving to scene with bci input
+        //currentValue = (currentValue - openBCIInput.classificationThreshold) / (1 - openBCIInput.classificationThreshold);
         float playTime = anim.GetCurrentAnimatorStateInfo(0).normalizedTime; //Value of 0 to 1 of the current playtime of animation
-        // below 0.5 is hand closing ---- abouve 0.5 is hand closing
 
-        float tempThreshold = 0.5f; //Threshold for when input is accepted
-        currentValue = (currentValue - tempThreshold) / (1 - tempThreshold); //Proportional value in range of threshold to 1
-
-        // If propotional value is 0.8 correspond to 0.4 in playTime value
-        // Need to check if playTime is less then the propotional value, if it is then keep playing handclose ani, 
-        // but if playTime is more then, then switch to other ani until playTime over 0.5 correspond with the proportional value
-
-        /* 
-         * Switch animation to playtime over 0.5 with 
-         * anim.SetBool("Open", false);
-         * anim.SetBool("Close", true);
-         * 
-         * Animation under 0.5 is
-         * anim.SetBool("Open", true);
-         * anim.SetBool("Close", false);
-         */
-        Debug.Log("Running: C " + currentValue + " t " + tempThreshold + " p " + playTime + " l " + lastValue);
-        if (lastValue <= currentValue)
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("SittingIdle"))
         {
-            lastValue = currentValue;
-            if (playTime < 0.5f)
+            anim.Play("HandSqueeze(Close)", 0);
+        }
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("HandSqueeze(Close)"))
+        {
+            if (playTime > currentValue)
             {
-                if (playTime < currentValue / 2)
-                {
-                    anim.SetBool("Open", false);
-                    anim.SetBool("Close", true);
-                }
-                else if (playTime > currentValue / 2)
-                {
-                    anim.SetBool("Open", true);
-                    anim.SetBool("Close", false);
-                }
+                anim.Play("HandSqueeze(Open)", 0, 1 - playTime);
             }
         }
-        else
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("HandSqueeze(Open)"))
         {
-            lastValue = currentValue;
-            if (playTime < 0.5f)
+            if (playTime >= 1 - currentValue)
             {
-                if (playTime < lastValue / 2)
-                {
-                    anim.SetBool("Open", false);
-                    anim.SetBool("Close", true);
-                }
-                else if (playTime > lastValue / 2)
-                {
-                    anim.SetBool("Open", true);
-                    anim.SetBool("Close", false);
-                }
+                anim.Play("HandSqueeze(Close)", 0, 1 - playTime);
             }
         }
     }
